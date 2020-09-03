7235d6144d91220418e63eaf9d89cc8572112c37
Matthew Jones <MJones@sessionm.lan>
2020-09-03 18:27:58 -0400

    Update Products

--- src/screens/Products/Products.tsx
+++ src/screens/Products/Products.tsx
@@ -17,17 +17,15 @@ const ScreensProducts: FunctionComponent = () => {
                         <p>Dal-Tile</p>
                         <p>Mannington</p>
                         <p>Marazzi</p>
-                        <p>Anderson Floors</p>
                         <p>Mullican Flooring</p>
                         <p>Bella Cera Floors</p>
+                        <p>Anderson Tuftex</p>
+                        <p>US Floors LLC</p>
                         <p>Karndean</p>
                         <p>Nova</p>
                         <p>We Cork</p>
-                        <p>US Floors LLC</p>
-                        <p>Quick Step</p>
                         <p>Tile Gallery PA</p>
                         <p>Shaw Floors</p>
-                        <p>BR1-11</p>
                     </Col>
                 </Row>
             </Container>
